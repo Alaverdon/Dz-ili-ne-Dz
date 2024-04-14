e4109dc2eca4e8dddd7b95b27c45718a603b9fa8
Alaverdon <158495791+Alaverdon@users.noreply.github.com>
2024-04-14 18:48:25 +0400

    extensions.py

--- extensions.py
+++ extensions.py
@@ -4,7 +4,7 @@ import json
 from dz.config import keys
 
 
-class ConvertionException(Exception):
+class ApiException(Exception):
     pass
 
 
@@ -12,20 +12,20 @@ class CryproConverter:
     @staticmethod
     def convert(quote: str, base: str, amount: str):
         if quote == base:
-            raise ConvertionException(f'Нельзя перевести одно и то же, Бе! {base}!')
+            raise ApiException(f'Нельзя перевести одно и то же, Бе! {base}!')
         try:
             quote_ticker = keys[quote]
         except KeyError:
-            raise ConvertionException(f'Пиши правильно, Бе! {quote}!')
+            raise ApiException(f'Пиши правильно, Бе! {quote}!')
 
         try:
             base_ticker = keys[base]
         except ValueError:
-            raise ConvertionException(f'Пиши правильно, Бе! {base}!')
+            raise ApiException(f'Пиши правильно, Бе! {base}!')
         try:
             amount = float(amount)
         except ValueError:
-            raise ConvertionException(f'Число не правильное, Бе! {amount}!')
+            raise ApiException(f'Число не правильное, Бе! {amount}!')
 
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
         total_base = json.loads(r.content)[keys[base]] * amount
